69e63862456379be53eccb0f94df29c81e7fd24e
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2023-07-01 19:47:11 +0530

    Create Fair Distribution of Cookies.cpp

---
+++ Fair Distribution of Cookies.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int n;
+    int result=INT_MAX;
+    void solve(int ind,vector<int>&cookies,vector<int>&child,int k){
+        if(ind>=n){
+            int unf=*max_element(child.begin(),child.end());
+            result=min(result,unf);
+            return ;
+        }
+        int cok=cookies[ind];
+        for(int i=0;i<k;i++){
+            child[i]+=cok;
+            solve(ind+1,cookies,child,k);
+            child[i]-=cok;
+
+        }
+    }
+    int distributeCookies(vector<int>& cookies, int k) {
+        n=cookies.size();
+        vector<int>child(k,0);
+        
+        solve(0,cookies,child,k);
+        return result;
+        
+    }
+};
