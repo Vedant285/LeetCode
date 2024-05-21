aaccb4f6fdb66b6d8a8f508f7dff9f238a976f84
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-05-21 19:59:08 +0530

    Time: 3 ms (65.11%) | Memory: 7.3 MB (80.89%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -4,7 +4,7 @@
 class Solution {
 public:
     int cnt = 0;
-    void solve(int col, vs& ans,int n, vi& t, vi& upper, vi& down) {
+    void solve(int col,int n, vi& t, vi& upper, vi& down) {
         if (col == n) {
             cnt ++;
             return;
@@ -14,24 +14,16 @@ public:
                 t[i] = 1;
                 upper[i+col] = 1;
                 down[n-1+col -i] =1;
-                ans [i][col] = 'Q';
-                solve(col+1, ans,n,t,upper,down );
+                solve(col+1,n,t,upper,down );
                 t[i] = 0;
                 upper[i+col] = 0;
                 down[n-1+col -i] =0;
-                ans [i][col] = '.';
             }
         }
     }
     int totalNQueens(int n) {
-        string s(n,'.');
-        vss board(n);
-        vs ans(n);
-        for (int i =0 ; i< n ; i++) {
-            ans [i] = s;
-        }
         vi t(n,0), upper(2*n -1), down(2*n -1);
-        solve(0, ans, n, t, upper, down);
+        solve(0, n, t, upper, down);
         return cnt;
     }
 };
