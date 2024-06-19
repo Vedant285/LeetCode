7d78782b9e60d3d82552df42a2d0c2ee3e891efd
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-06-19 19:07:01 +0530

    Time: 205 ms (5.04%) | Memory: 69 MB (65.37%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,38 +1,39 @@
+#define ll long long
 class Solution {
 public:
-    bool check(vector<int>&arr, int m, int k, int mid ) {
-        int cnt = 0;
-        int mini = 0;
-        for (int i = 0 ; i < size(arr) ; i++) {
-            if (arr[i] <= mid) {
-                cnt ++;
+    bool check(vector<int>& nums, int m, int k, int idx) {
+        ll sum = 0;
+        ll ans = 0;
+        for (int i = 0 ; i < nums.size(); i++) {
+            if (nums[i] <= idx) {
+                sum ++;
             }
-            else{
-                mini += (cnt/k);
-                cnt = 0;
+            else {
+                ans += (sum / k);
+                sum = 0;
             }
         }
-        mini += (cnt/k);
-        return m <= mini;
+        ans += (sum  / k);
+        return ans >= m;   
     }
 
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n = size(bloomDay);
-        if ((long long)m* (long long)k > n) {
+        int n = bloomDay.size();
+        if ((ll)k * (ll)m > n) {
             return -1;
         }
-        int s = *min_element(begin(bloomDay), end(bloomDay));
-        int l = *max_element(begin(bloomDay), end(bloomDay));
-        int ans = INT_MAX;
-        while (s <= l) {
-            int mid = s + (l - s)/2;
-            if(check(bloomDay, m, k, mid)) {
-                l = mid - 1;
+        ll s = *min_element(bloomDay.begin(), bloomDay.end());
+        ll l = *max_element(bloomDay.begin(), bloomDay.end());
+        while( s < l) {
+            ll mid = s + ( l - s )/2;
+            if (check(bloomDay, m, k, mid) ) {
+                l = mid ;
             }
-            else{
-                s = mid+1;
+            else {
+                s = mid + 1;
             }
         }
+        
         return s;
     }
 };
