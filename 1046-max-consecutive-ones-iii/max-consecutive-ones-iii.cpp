c0771dde8131b748135b33f2eecc53fc0869cda0
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-07-23 15:34:09 +0530

    Time: 49 ms (17.58%) | Memory: 58.3 MB (6.54%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -5,7 +5,7 @@ public:
         int l = 0, r = 0, zero = 0;
         while (r < nums.size()) {
             if (nums[r] == 0) zero++;
-            while (zero > k) {
+            if (zero > k) {
                 if(nums[l] == 0) {
                     zero--;
                 }
@@ -13,8 +13,8 @@ public:
             }
             if(zero <= k) {
                 maxi = max(maxi, r - l + 1);
-                r++;
             }
+            r++;
         }
         return maxi;
     }
