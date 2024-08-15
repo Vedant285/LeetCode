16bf824d274515322b240b81bf152527e730def9
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-08-15 11:40:35 +0530

    Time: 66 ms (81.16%) | Memory: 86.1 MB (32.71%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -3,25 +3,22 @@ public:
     bool lemonadeChange(vector<int>& bills) {
         int f = 0, t = 0;
         for (auto it : bills) {
-            if (it == 5) {
-                f++;
-            }
+            if (it == 5) f++;
             else if (it == 10) {
-                if (f > 0) {
+                if(f >= 1) {
                     f--;
                     t++;
                 }
-                else {
-                    return false;
-                }
+                else if (t >= 1) t--;
+                else return false;
             }
             else {
-                if(f && t) {
-                    f--;
+                if (f >= 1 && t >= 1) {
                     t--;
+                    f--;
                 }
-                else if(f >= 3) {
-                    f -= 3;
+                else if (f >= 3 && t == 0) {
+                    f-= 3;
                 }
                 else {
                     return false;
