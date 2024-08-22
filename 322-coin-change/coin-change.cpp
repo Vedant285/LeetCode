37e2bc46b974b75655e0565111eb8033e09aa1b0
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-08-22 20:36:28 +0530

    Time: 31 ms (65.44%) | Memory: 20.6 MB (49.88%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     int coinChange(vector<int>& arr, int amo) {
         int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(amo + 1));
-        
+        //vector<vector<int>> dp(n, vector<int>(amo + 1));
+        vector<int> prev(amo + 1), curr(amo + 1);
         for (int i = 0; i <= amo; i++) {
-            if(i % arr[0] == 0) dp[0][i] = i / arr[0];
-            else dp[0][i] = 1e9;
+            if(i % arr[0] == 0) prev[i] = i / arr[0];
+            else prev[i] = 1e9;
         }
         for (int i = 1; i < n; i++) {
             for (int t = 0; t <= amo; t++) {
 
-                int nottake = dp[i - 1][t];
+                int nottake = prev[t];
                 int take = 1e9;
-                if(arr[i] <= t) take = 1 + dp[i][t - arr[i]];
+                if(arr[i] <= t) take = 1 + curr[t - arr[i]];
 
-                dp[i][t] = min(take, nottake);
+                curr[t] = min(take, nottake);
             }
+            prev = curr;
         }
 
-        if (dp[n - 1][amo] >= 1e9) return -1;
-        return dp[n - 1][amo];
+        if (prev[amo] >= 1e9) return -1;
+        return prev[amo];
     }
 };
