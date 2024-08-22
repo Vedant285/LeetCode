281e5c786d8e27bdc26097cd8ad51eff6d213310
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-08-22 20:57:35 +0530

    Time: 12 ms (46.88%) | Memory: 8.8 MB (89.15%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
-    int solve(int i,int t, vector<int>& arr ) {
-        if (i == 0) {
-            return (t % arr[0] == 0);
-        }
-        int nottake = solve(i - 1, t, arr);
-        int take = 0;
-        if (arr[i] <= t) take = solve(i, t - arr[i], arr);
-        return take + nottake;
-    }
     int change(int amount, vector<int>& arr) {
         int n = arr.size();
         //vector<vector<long>> dp(n, vector<long>(amount + 1, 0));
-        vector<int> prev(amount + 1, 0), curr(amount + 1, 0);
+        vector<int> prev(amount + 1, 0);
         for (int i = 0; i <= amount; i++) {
             if (i % arr[0] == 0) prev[i] = 1;
         }
@@ -21,10 +12,9 @@ public:
             for (int t = 0; t <= amount; t++) {
                 int nottake = prev[t];
                 int take = 0;
-                if (arr[i] <= t) take = curr[t - arr[i]];
-                curr[t] = take + nottake;
+                if (arr[i] <= t) take = prev[t - arr[i]];
+                prev[t] = take + nottake;
             }
-            prev = curr;
         }
         return prev[amount];
     }
