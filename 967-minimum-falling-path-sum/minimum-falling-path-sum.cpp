1454d5ea1e34f3227541b50ee2fa0bb4f1546e63
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-08-22 11:18:10 +0530

    Time: 7 ms (94.66%) | Memory: 13.2 MB (89.60%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp (n, vector<int>(n));
-        for (int i = 0; i < n; i++) dp[0][i] = matrix[0][i];
+        //vector<vector<int>> dp (n, vector<int>(n));
+        vector<int> prev(n, 0), curr(n, 0);
+        for (int i = 0; i < n; i++) prev[i] = matrix[0][i];
 
-        for (int i = 1; i < n; i ++) {
+        for (int i = 1; i < n; i++) {
             for (int j = 0; j < n; j++){
 
-                int up = matrix[i][j] + dp[i - 1][j];
+                int up = matrix[i][j] + prev[j];
 
                 int leftDiag = matrix[i][j];
-                if(j > 0) leftDiag += dp[i - 1][j - 1];
+                if(j > 0) leftDiag += prev[j - 1];
                 else leftDiag += (1e9);
 
                 int rightDiag = matrix[i][j];
-                if(j + 1 < n) rightDiag += dp[i - 1][j + 1];
+                if(j + 1 < n) rightDiag += prev[j + 1];
                 else rightDiag += (1e9);
 
-                dp[i][j] = min(up, min(leftDiag, rightDiag));
+                curr[j] = min(up, min(leftDiag, rightDiag));
             }
+            prev = curr;
         }
-        int mini = *min_element(dp[n- 1].begin(), dp[n - 1].end());
+        int mini = *min_element(prev.begin(), prev.end());
         return mini;
     }
 };
