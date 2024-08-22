e8d69ea2fb1f00907b1127deffdc6ca27bcf1d36
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-08-22 14:16:52 +0530

    Time: 357 ms (8.17%) | Memory: 16.4 MB (67.45%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -16,7 +16,20 @@ public:
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if(sum % 2 == 1) return false;
         int n = size(nums);
-        vector<vector<int>>dp(n + 1, vector<int>((sum/2) + 1, -1));
-        return solve(n - 1, sum / 2, nums, dp);
+        int k = sum / 2;
+        vector<vector<bool>>dp(n, vector<bool>(k + 1, false));
+
+        for (int i = 0; i < n; i++) dp[i][0] = true;
+        if(nums[0] <= k) dp[0][nums[0]] = true;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= k; j++) {
+                int nottake = dp[i - 1][j];
+                int take = false;
+                if(nums[i] <= j) take = dp[i - 1][j - nums[i]];
+                dp[i][j] = nottake | take;
+            }
+        }
+        return dp[n - 1][k];
     }
 };
