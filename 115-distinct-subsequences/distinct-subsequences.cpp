1ac72a395c61efe45a7e62110f822825bb3f7a45
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-08-23 09:38:04 +0530

    Time: 11 ms (86.47%) | Memory: 7.9 MB (93.12%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -12,18 +12,18 @@ public:
     }
     int numDistinct(string s, string t) {
         int n = s.size(), m = t.size();
-        vector<vector<int>>dp (n + 1, vector<int>(m + 1, 0));
-        for (int i = 0; i <= n; i++) dp[i][0] = 1;
-        for (int i = 1; i <= m; i++) dp[0][i] = 0;
-
+        //vector<vector<int>>dp (n + 1, vector<int>(m + 1, 0));
+        vector<int> prev(m + 1, 0);
+        //for (int i = 0; i <= n; i++) dp[i][0] = 1;
+        //for (int i = 1; i <= m; i++) dp[0][i] = 0;
+        prev[0] = 1;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++){
-                if(s[i - 1] == t[j - 1]) {
-                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod;
-                }
-                else dp[i][j] = dp[i - 1][j];
+            for (int j = m; j >= 1; j--){
+                if(s[i - 1] == t[j - 1])
+                    prev[j] = (prev[j - 1] + prev[j]) % mod;
             }
+                
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
