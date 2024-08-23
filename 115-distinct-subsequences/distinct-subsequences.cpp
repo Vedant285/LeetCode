a7c51bf5430576b16c36c71df5c42c74533eb405
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-08-23 09:30:53 +0530

    Time: 17 ms (66.38%) | Memory: 20 MB (62.08%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int mod = 1e9;
     int solve(int i, int j, string s, string t, vector<vector<int>>& dp){
         if (j < 0) return 1;
         if(i < 0) return 0;
@@ -11,7 +12,18 @@ public:
     }
     int numDistinct(string s, string t) {
         int n = s.size(), m = t.size();
-        vector<vector<int>>dp (n + 1, vector<int>(m + 1, -1));
-        return solve(n - 1, m - 1, s, t, dp);
+        vector<vector<int>>dp (n + 1, vector<int>(m + 1, 0));
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+        for (int i = 1; i <= m; i++) dp[0][i] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++){
+                if(s[i - 1] == t[j - 1]) {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod;
+                }
+                else dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][m];
     }
 };
