a04ef7100c455c50e66ff8fce425c7edcbed4331
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-08-23 20:01:28 +0530

    Time: 38 ms (25.07%) | Memory: 30.2 MB (6.80%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -16,15 +16,16 @@ public:
         vector<vector<int>> dp(n + 1, vector<int>(n + 1));
         for (int i = 0; i <= n; i++)  dp[i][0] = 0;
         for (int i = 0; i <= n; i++)  dp[0][i] = 0;
-
+        vector<int> prev(n + 1, 0), curr(n + 1, 0);
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if(s[i - 1] == k[j - 1]) 
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    curr[j] = 1 + prev[j - 1];
 
-                else dp[i][j] = max(dp[i - 1][j],  dp[i][j - 1]);
+                else curr[j] = max(prev[j],  curr[j - 1]);
             }
+            prev = curr;
         }
-        return n - dp[n][n];
+        return n - prev[n];
     }
 };
