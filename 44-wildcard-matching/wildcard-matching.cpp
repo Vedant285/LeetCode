8bab045130cd453d4b99494e343288153d06eebf
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-08-29 19:50:36 +0530

    Time: 71 ms (24.15%) | Memory: 16.7 MB (60.64%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool solve(int i, int j, string &s, string &p, vector<vector<int>>& dp) {
         if(i == 0 && j == 0) return true;
-        if( j == 0 && i > 0) return false;
+        if(j == 0 && i > 0) return false;
 
         if(i == 0 &&  j > 0) {
             for (int k = 1; k <= j; k++) {
@@ -21,7 +21,35 @@ public:
     }
     bool isMatch(string s, string p) {
         int n = s.size(), m = p.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return solve(n, m, s, p, dp);
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 0; i <= n; i++) dp[i][0] = 0;
+        for (int j = 0; j <= m; j++) {
+            bool flag = true;
+            for (int k = 1; k <= j; k++) {
+                if(p[k - 1] != '*'){ 
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][j] = flag;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if(s[i - 1] == p[j - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if(p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+                else 
+                    dp[i][j] = false;
+
+            }
+        }
+
+        return dp[n][m];
     }
 };
