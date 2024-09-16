861ac3724a422d27c8a4dac714ffc35ab9ed9baf
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-09-16 10:01:23 +0530

    Time: 162 ms (33.33%) | Memory: 145.5 MB (100.00%) - LeetSync

--- 3518-maximum-multiplication-score/maximum-multiplication-score.cpp
+++ 3518-maximum-multiplication-score/maximum-multiplication-score.cpp
@@ -17,18 +17,18 @@ public:
     }
     long long maxScore(vector<int>& a, vector<int>& b) {
         int n = a.size(), m = b.size();
-        vector<vector<ll>> dp(n + 1, vector<ll>(m + 1, -1e9));
+        vector<ll>prev(m + 1, 0), curr(m + 1, -1e9);
 
-        for (int j = 0; j <= m; j++) dp[0][j] = 0;
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                ll take =(a[i - 1]*1LL * b[j - 1]*1LL) + dp[i - 1][j - 1];
-                ll nottake = dp[i][j - 1];
-                dp[i][j] = max(take, nottake);
+                ll take =(a[i - 1]*1LL * b[j - 1]*1LL) + prev[j - 1];
+                ll nottake = curr[j - 1];
+                curr[j] = max(take, nottake);
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
