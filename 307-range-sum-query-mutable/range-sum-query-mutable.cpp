2fabffcc9b2f95acc631b09ac78ba17eb51d8e83
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-09-25 09:11:30 +0530

    Time: 377 ms (53.87%) | Memory: 177.7 MB (65.88%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,48 +1,37 @@
-class BIT{
+class NumArray {
 public:
-    vector<int> fen;
     int n;
-    BIT(int N = 0) {
-        n = N;
-        fen.resize(N + 1, 0);
+    vector<int>bit, arr;
+    NumArray(vector<int>& nums) {
+        arr = nums;
+        this->n = nums.size();
+        bit.resize(n + 1, 0);
+        for (int i = 0;i < n; i++) {
+            add(i + 1, nums[i]);
+        }    
     }
-    void addv(int t, int i) {
+    void add(int i, int nums) {
         while (i <= n) {
-            fen[i] += t;
+            bit[i] += nums;
             i += (i & (-i));
         }
     }
-    int getsum(int i) {
-        int sum = 0;
-        while(i > 0) {
-            sum += fen[i];
-            i = (i - (i & (-i)));
-        }
-        return sum;
-    }
-};
-
-class NumArray {
-    BIT obj;
-public:
-    int n;
-    vector<int> arr;
-    NumArray(vector<int>& nums) {
-        this->n = nums.size();
-        this->obj = BIT(n);
-        this->arr = nums;
-        for (int i = 0; i < n; i++) {
-            obj.addv(nums[i], i + 1);
-        }
-    }
-    
     void update(int index, int val) {
         int diff = val - arr[index];
-        obj.addv(diff, index + 1);
         arr[index] = val;
+        add(index + 1, diff);
     }
     
+    int sum(int i) {
+        int s = 0;
+        while (i > 0) {
+            s += bit[i];
+            i -= (i & (-i));
+        }
+        return s;
+    }
+
     int sumRange(int left, int right) {
-        return obj.getsum(right + 1) - obj.getsum(left);
+        return sum(right + 1) - sum(left);
     }
 };
