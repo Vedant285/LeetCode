53de696f985c4054d5fc0baa95bf9bb57a4836ba
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-10-20 18:30:39 +0530

    Time: 1922 ms (5.01%) | Memory: 638.9 MB (12.04%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    int n;
-    bool check(string &a,string &b){
-        int M=a.size();
-        int N=b.size();
-
-        if(M>=N || N-M!=1){
-            return false;
-        }
-        int i=0,j=0;
-        while(i<M && j<N){
-            if(a[i]==b[j])
-                i++;
+    bool check(string s1, string s2) {
+        if(s1.size() + 1 != s2.size()) return 0;
+        int j = 0, i = 0;
+        while(i < s1.size() && j < s2.size()) {
+            if(s1[i] == s2[j]) {
+                i++; 
+            }
             j++;
         }
-    return i==M;
-    }
-    static bool compare (string &s1, string &s2){
-        return s1.size()<s2.size();
-    }
+        return i == s1.size();
+    };
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(),words.end(),compare);
-        n=words.size();
-        vector<int>dp(n,1);
-        int maxl=1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(check(words[j],words[i])){
-                    dp[i]=max(dp[i],dp[j]+1);
-                    maxl=max(maxl,dp[i]);
+        int n = size(words);
+        vector<int> dp(n, 1);
+        sort(words.begin(), words.end(), [&](string &s1, string &s2) {
+            return s1.size() < s2.size();
+        });
+        int maxi = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if(check(words[j], words[i])) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                    maxi = max(maxi, dp[i]);
                 }
             }
         }
-        return maxl;
+        return maxi;
     }
 };
