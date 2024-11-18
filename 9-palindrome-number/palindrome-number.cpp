724025e2910e22cdbf049a0deaa457176af61843
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-11-18 19:21:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (13.51%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) {
-            return false;
-        }
+        if(x < 0) return false;
         string s = to_string(x);
-        int n = size(s);
-        for (int i = 0 ; i < size(s)/2 ; i++) {
-            if (s[i] != s[n - i - 1]) {
-                return false;
-            }
+        int n = s.size();
+        for (int i = 0; i < n / 2; i++) {
+            if(s[i] != s[n - i - 1]) return 0;
         }
-        return true;
+        return 1;
     }
 };
