cf49a757512baf37cc80d3d8c1d309e949f567c4
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2024-12-26 16:01:09 +0530

    Time: 1008 ms (5.04%) | Memory: 11.6 MB (74.02%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int cnt;
-    void solve(int i, vector<int>& nums, int target){
-        if(i < 0) {
-            if(target == 0) cnt++;
-            return;
+    
+    int solve(int i, int sum, vector<int>& nums, int target) {
+        if(i == nums.size()) {
+            if(sum == target) return 1;
+            return 0;
         }
+        int takeadd = solve(i + 1, sum + nums[i], nums, target);
+        int takesub = solve(i + 1, sum - nums[i], nums, target);
+        return takeadd + takesub;
+    }
 
-        solve(i - 1, nums, target + nums[i]);
-        solve(i - 1, nums, target - nums[i]);
-    } 
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = size(nums);
-        solve(n - 1, nums, target);
-        return cnt;
+        return solve(0, 0, nums, target);
     }
 };
