7d152519403386892c0a5fa916c27abcc6173427
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2025-01-01 19:51:27 +0530

    Time: 2 ms (24.89%) | Memory: 8.6 MB (52.60%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
+
     int maxScore(string s) {
         int one = 0;
-        for (auto it : s) {
-            if( it == '1') one++;
-        }
-        int n = s.size();
+        for (auto it : s) 
+            if(it == '1') one++;
+
         int ans = 0;
         int z = 0;
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < s.size(); i++) {
             if(s[i - 1] == '0') {
                 z++;
-                int t = one + z;
+                int t = z + one;
                 ans = max(ans, t);
             }
             else {
                 one--;
-                int t = one + z;
+                int t = z + one;
                 ans = max(ans, t);
             }
-        }
-        return ans;
+        }   
+        return ans;     
     }
 };
