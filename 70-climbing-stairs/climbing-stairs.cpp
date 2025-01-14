64e03e21d64c58e59190d81a96939fb5a6f4739b
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2025-01-14 13:07:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (40.04%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int solve(int i, vector<int> &dp) {
-        if (i <= 1) {
-            return 1;
-        }
-        if (dp[i] != -1) return dp[i];
-        return dp[i] = solve(i - 1, dp) + solve(i - 2, dp);
+    int solve(int n, vector<int>& dp) {
+        //if(n < 0) return 0;
+        if(n <= 1) return 1;
+        if(dp[n] != -1 ) return dp[n];
+
+        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp); 
     }
     int climbStairs(int n) {
         vector<int> dp(n + 1, -1);
