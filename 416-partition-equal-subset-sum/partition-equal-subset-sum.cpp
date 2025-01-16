e579dc69bab9cc74defd31d2fdbdd5b239438786
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2025-01-16 11:37:11 +0530

    Time: 131 ms (51.07%) | Memory: 100.3 MB (35.72%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-    bool solve(int i, int k, vector<int>& nums, vector<vector<int>>& dp) {
-        
+    bool solve(int i, int sum, vector<int>& arr, vector<vector<int>>& dp) 
+    {
+        if(sum == 0) return 1;
         if(i == 0) {
-            if(k == 0) return true;
-            return nums[0] == k;
+            if(arr[i] == sum) return 1;
+            return 0;
         }
-        if(dp[i][k] != -1) return dp[i][k];
-        int nottake = solve(i - 1, k, nums, dp);
-        int take = false;
-        if(nums[i] <= k) take = solve(i - 1, k - nums[i], nums, dp);
-        return dp[i][k] = nottake | take;
+        if(dp[i][sum] != -1) return dp[i][sum];
+        int take = (arr[i] <= sum) ? solve(i - 1, sum - arr[i], arr, dp): 0;
+        int nottake = solve(i - 1, sum, arr, dp);
+        return dp[i][sum] = take || nottake;
     }
-    bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if(sum % 2 == 1) return false;
-        int n = size(nums);
-        int k = sum / 2;
-        //vector<vector<bool>>dp(n, vector<bool>(k + 1, false));
-        vector<int> prev(k + 1, 0), curr(k + 1, 0);
-        prev[0] = true;
-        if(nums[0] <= k) prev[nums[0]] = true;
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j <= k; j++) {
-                int nottake = prev[j];
-                int take = false;
-                if(nums[i] <= j) take = prev[j - nums[i]];
-                curr[j] = nottake | take;
-            }
-            prev = curr;
-        }
-        return prev[k];
+    bool canPartition(vector<int>& nums) {
+        long long sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum %2 != 0) return 0;
+        sum /= 2;
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
+        return solve(n - 1, sum, nums, dp);
     }
 };
