0eb35a6ded74c9fe7888e4a34e7f08283bb3e5e4
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2025-01-16 11:44:22 +0530

    Time: 75 ms (81.35%) | Memory: 15.5 MB (79.30%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -5,21 +5,21 @@ public:
         if(s %2 != 0) return 0;
         s /= 2;
         int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(s + 1, 0));
+        vector<int>prev(s + 1, 0), curr(s + 1, 0);
 
         for (int sum = 0; sum <= s; sum++) {
-            if(arr[0] == sum) dp[0][sum] = 1;
+            if(arr[0] == sum) prev[sum] = 1;
         }
 
 
         for (int i = 1; i < n; i++) {
             for (int sum = 0; sum <= s; sum++) {
-                    int take = (arr[i] <= sum) ? dp[i - 1][sum - arr[i]]: 0;
-                    int nottake = dp[i - 1][sum];
-                    dp[i][sum] = take || nottake;
+                int take = (arr[i] <= sum) ? prev[sum - arr[i]]: 0;
+                int nottake = prev[sum];
+                curr[sum] = take || nottake;
             }
+            prev = curr;
         }
-
-        return dp[n - 1][s];
+        return prev[s];
     }
 };
