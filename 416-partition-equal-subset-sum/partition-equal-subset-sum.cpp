a046387eb79d1d613d23b35bca362a70906b7857
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2025-01-16 11:43:11 +0530

    Time: 148 ms (35.63%) | Memory: 100.2 MB (41.36%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    bool solve(int i, int sum, vector<int>& arr, vector<vector<int>>& dp) 
-    {
-        if(sum == 0) return 1;
-        if(i == 0) {
-            if(arr[i] == sum) return 1;
-            return 0;
-        }
-        if(dp[i][sum] != -1) return dp[i][sum];
-        int take = (arr[i] <= sum) ? solve(i - 1, sum - arr[i], arr, dp): 0;
-        int nottake = solve(i - 1, sum, arr, dp);
-        return dp[i][sum] = take || nottake;
-    }
+    bool canPartition(vector<int>& arr) {
+        long long s = accumulate(arr.begin(), arr.end(), 0);
+        if(s %2 != 0) return 0;
+        s /= 2;
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(s + 1, 0));
 
-    bool canPartition(vector<int>& nums) {
-        long long sum = accumulate(nums.begin(), nums.end(), 0);
-        if(sum %2 != 0) return 0;
-        sum /= 2;
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
-        return solve(n - 1, sum, nums, dp);
+        for (int sum = 0; sum <= s; sum++) {
+            if(arr[0] == sum) dp[0][sum] = 1;
+        }
+
+
+        for (int i = 1; i < n; i++) {
+            for (int sum = 0; sum <= s; sum++) {
+                    int take = (arr[i] <= sum) ? dp[i - 1][sum - arr[i]]: 0;
+                    int nottake = dp[i - 1][sum];
+                    dp[i][sum] = take || nottake;
+            }
+        }
+
+        return dp[n - 1][s];
     }
 };
