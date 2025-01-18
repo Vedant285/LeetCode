0347cbec6c51265481b682d2980361763381310d
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2025-01-18 11:53:37 +0530

    Time: 11 ms (82.37%) | Memory: 10.6 MB (79.50%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -3,21 +3,22 @@ public:
     int change(int amount, vector<int>& arr) {
         int n = arr.size();
         //long long mod = 2e9 + 7;
-        vector<vector<double>>dp(n, vector<double>(amount+1, 0));
+        vector<double>prev(amount+1, 0), curr(amount + 1, 0);
         
         for (int i = 0; i <= amount; i++) {
-            if(i % arr[0] == 0) dp[0][i] = 1;
+            if(i % arr[0] == 0) prev[i] = 1;
         }
 
         for (int i = 1; i < n; i++) {
             for (int t = 0; t <= amount; t++) {
                 
-                double ta = (arr[i] <= t)? dp[i][t - arr[i]]: 0;
-                double nt = dp[i - 1][t];
-                dp[i][t] = (ta + nt);
+                double ta = (arr[i] <= t)? curr[t - arr[i]]: 0;
+                double nt = prev[t];
+                curr[t] = (ta + nt);
             }
+            prev = curr;
         }
 
-        return (int)dp[n - 1][amount];
+        return (int)prev[amount];
     }
 };
