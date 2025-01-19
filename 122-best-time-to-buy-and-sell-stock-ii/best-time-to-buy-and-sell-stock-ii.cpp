9f4b79f84220acb9a6167d37bbf1023da873815d
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2025-01-19 10:04:59 +0530

    Time: 7 ms (15.90%) | Memory: 22.3 MB (6.53%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
-    int solve(int i, int c, vector<int>& arr, int n, vector<vector<int>>& dp) {
-
+    int n;
+    int solve(int i, int bos, vector<int>& arr, vector<vector<int>>& dp) {
         if(i == n) return 0;
-        if(dp[i][c] != -1) return dp[i][c];
-        int pr = 0;
-        if(c == 0) {
-            pr = max(-arr[i] + solve(i + 1, 1, arr, n, dp), solve(i + 1, 0, arr, n, dp));
+        if(dp[i][bos] != -1) return dp[i][bos];
+
+        if(bos == 0) {
+            return  dp[i][bos] = max(-arr[i] + solve(i + 1, 1, arr, dp), solve(i + 1, 0, arr, dp));
         }
-        else {
-            pr = max(arr[i] + solve(i + 1, 0, arr, n, dp), solve(i + 1, 1, arr, n, dp));
-        }
-        return dp[i][c] = pr;
+        return dp[i][bos] = max(arr[i] + solve(i + 1, 0, arr, dp), solve(i + 1, 1, arr, dp));
     }
-    int maxProfit(vector<int>& arr) {
-        int n = arr.size();
-        //vector<vector<int>> dp(n + 1, vector<int>(3, 0));
-        vector<int> pre(3, 0), curr(3, 0);
-        for (int i = n - 1; i >= 0; i--) {
-            for (int c = 0; c <= 1; c++) {
-                int pr = 0;
-                if(c == 0) {
-                    pr = max(-arr[i] + pre[1], pre[0]);
-                }
-                else {
-                    pr = max(arr[i] + pre[0], pre[1]);
-                }
-                curr[c] = pr;
-            }
-            pre = curr;
-        }
-        return pre[0];
+    int maxProfit(vector<int>& prices) {
+        n = prices.size();
+        vector<vector<int>>dp(n, vector<int>(2, -1));
+        return solve(0, 0, prices, dp);
     }
 };
