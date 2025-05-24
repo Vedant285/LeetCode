b824e66332a52593a0ae75ee8c7627d8f71e3e0e
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2025-05-24 19:21:15 +0530

    Time: 0 ms (100.00%) | Memory: 33 MB (80.27%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        std::vector<int> indices;
+
+    for (int i = 0; i < words.size(); ++i) {
+
+        if (words[i].find(x) != std::string::npos) {
+
+            indices.push_back(i);
+
+        }
+
+    }
+
+    return indices;
+    }
+};
