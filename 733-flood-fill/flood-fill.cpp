32491dcd536d0accf9820c4c3672519b2407d342
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2025-10-22 16:53:16 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (45.80%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,30 +1,24 @@
 class Solution {
-public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int prev = image[sr][sc];
-        if(prev == color) return image;
-        queue<pair<int, int>> pq;
-        int n = image.size(), m = image[0].size();
-
-        pq.push({sr, sc});
-        while (!pq.empty()) {
-            auto it = pq.front();
-            pq.pop();
-            int i = it.first;
-            int j = it.second;
-
-            image[i][j] = color;
-            int dr[] = {-1, 0, 1, 0};
-            int dc[] = {0, -1, 0, 1};
-
-            for (int k = 0; k < 4; k++) {
-                int newr = i + dr[k];
-                int newc = j + dc[k];
-                if(newr >= 0 && newc >= 0 && newr < n && newc < m && image[newr][newc] == prev) {
-                    pq.push({newr, newc});
-                }
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int k){
+        int n=image.size();
+        int m=image[0].size();
+        image[sr][sc]=color;
+        int dr[]={-1,0,1,0};
+        int dc[]={0,-1,0,1};
+        for(int i=0;i<4;i++){
+            int nr=dr[i]+sr;
+            int nc=dc[i]+sc;
+            if(nr>=0 && nc>=0 && nr<n && nc<m && image[nr][nc]!=color && image[nr][nc]==k ){
+                dfs(image,nr,nc,color,k);
             }
         }
+    }
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n=image.size();
+        vector<vector<int>>vis(n,vector<int>(n,0));
+        int k=image[sr][sc];
+        dfs(image,sr,sc,color,k);
         return image;
     }
 };
