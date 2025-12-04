534408e1a74df2c09e22eb389cfd51cb4c2ebdef
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2025-12-04 19:35:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (82.21%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
+    bool kbit(int n, int i){
+        return 1 & (n >> i);
+    }
+    int check(int n) {
+        int cnt = 0;
+        for(int i=0;i<32;i++) {
+            if(kbit(n, i)) cnt++;
+        }
+        return cnt;
+    }
     bool isPowerOfTwo(int n) {
-        return (n>0 && (n& n-1)==0);
+        if(n< 0) return 0;
+        return check(n) ==1;
     }
 };
