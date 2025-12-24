dcf86c8c03fa91ca0a13218a4adf823eeb7c768a
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2025-12-24 11:26:05 +0530

    Time: 41 ms (87.49%) | Memory: 29.1 MB (45.81%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,27 +4,26 @@ public:
         vector<vector<int>> ans;
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        set<vector<int>> st;
+
         for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             int j = i+1;
             int k = n - 1;
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
-                if(sum ==0) {
-                    st.insert({nums[i], nums[j], nums[k]});
-                }
                 if(sum > 0) k--;
                 else if(sum < 0) j++;
                 else {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+
                     k--;
                     j++;
+
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
                 }
             }
         }
-        for (auto &it : st) {
-            ans.push_back(it);
-        }
-
         return ans;
     }
 };
