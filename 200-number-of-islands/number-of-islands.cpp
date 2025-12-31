1d09cc01046fe14a98fb6e18313014a0309520c6
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2025-12-31 13:53:07 +0530

    Time: 28 ms (49.07%) | Memory: 16.2 MB (88.72%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,29 +1,30 @@
 class Solution {
-public:
-    void dfs(vector<vector<char>>& grid, int i, int j) {
+    
+    void dfs(int i, int j, vector<vector<char>>& grid) {
         grid[i][j] = '0';
-        int n = size(grid);
-        int m = size(grid[0]);
         int dr[] = {-1, 0, 1, 0};
-        int dc[] = {0, 1, 0, -1};
+        int dc[] = {0, -1, 0, 1};
         for (int k = 0; k < 4; k++) {
-            int nr = dr[k] + i;
-            int nc = dc[k] + j;
-            if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == '1') dfs(grid, nr, nc);
+            int nr = i + dr[k];
+            int nc = j + dc[k];
+            if(nr >= 0 && nc >= 0 && nr < grid.size() && nc < grid[0].size() && grid[nr][nc] == '1') {
+                dfs(nr, nc, grid);
+            }
         }
     }
+
+public:
     int numIslands(vector<vector<char>>& grid) {
         int cnt = 0;
-        int n = size(grid);
-        int m = size(grid[0]);
+        int n = grid.size(), m = grid[0].size();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if(grid[i][j] == '1') {
+                    dfs(i, j, grid);
                     cnt++;
-                    dfs(grid, i, j);
-                }
+                } 
             }
-        }
+        }   
         return cnt;
     }
 };
