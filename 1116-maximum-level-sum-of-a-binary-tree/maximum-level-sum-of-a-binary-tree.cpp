77d5bf4e61f4c9e5950200ebff60d5d2d8ec4eff
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2026-01-06 16:40:02 +0530

    Time: 4 ms (57.77%) | Memory: 109.5 MB (59.06%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -1,43 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
-        int maxiL=0,level=0;
-        int maxiP=INT_MIN;
-        queue<TreeNode*>pq;
-        pq.push(root);
-        if(!root)return 0;
-        while(!pq.empty()){
+        int maxi = INT_MIN;
+        int ans = 0;
+        if(!root) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int level = 0;
+        while (!q.empty()) {
+            int sz = q.size();
+            int sum = 0;
             level++;
-            int curr=0;
-            int n=pq.size();
-            for(int i=0;i<n;i++){
-                auto node=pq.front();
-                pq.pop();
-                curr+=node->val;
-                if(node->left){
-                    pq.push(node->left);
-                }
-                if(node->right){
-                    pq.push(node->right);
-                }
+            while (sz--) {
+                auto it = q.front();
+                q.pop();
+                sum += it->val;
+                if(it->left) q.push(it->left);
+                if(it->right) q.push(it->right);
             }
-            if(maxiP<curr){
-                maxiP=curr;
-                maxiL=level;
+            if(sum > maxi) {
+                maxi = sum;
+                ans = level;
             }
-            
         }
-        return maxiL;
+        return ans;
     }
 };
