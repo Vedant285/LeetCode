6087bd0a716049c151529d1c64f52b78c62ef82e
Vedant Shukla <91669901+Vedant285@users.noreply.github.com>
2026-01-08 11:28:08 +0530

    Time: 11 ms (88.40%) | Memory: 18 MB (32.40%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -4,7 +4,7 @@ public:
     int solve(int i, int j, int n, int m, vector<int>& nums1, vector<int>& nums2, vector<vector<int>>&dp) {
         if(i == n || j == m) return -1e9;
         if(dp[i][j] != -1) return dp[i][j];
-        
+
         int val = nums1[i] * nums2[j];
         int takeij = solve(i + 1, j + 1, n, m, nums1, nums2, dp) + val;
         int takei = solve(i + 1, j, n, m, nums1, nums2, dp);
@@ -16,7 +16,21 @@ public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        vector<vector<int>>dp(n, vector<int>(m, -1));
-        return solve(0, 0, n, m, nums1, nums2, dp);
+        vector<vector<int>>dp(n + 1, vector<int>(m + 1, -1e9));
+
+
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                
+                int val = nums1[i] * nums2[j];
+                int takeij = dp[i + 1][j + 1] + val;
+                int takei = dp[i + 1][j];
+                int takej = dp[i][j + 1];
+                dp[i][j] = max({val, takeij, takej, takei});
+            }
+        }
+
+        return dp[0][0];
     }
 };
